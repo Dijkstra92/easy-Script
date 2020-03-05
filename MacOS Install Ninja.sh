4a450640d7ae6ae42bf93d703dbeb4b2aa800ca6
Dijkstra92 <35580539+Dijkstra92@users.noreply.github.com>
2020-03-05 22:53:00 +0100

    Update MacOS Install Ninja.sh

--- MacOS Install Ninja.sh
+++ MacOS Install Ninja.sh
@@ -3,7 +3,9 @@
 # Check if Teams service is running else it downloads and installs the agent
 
 AppName=Teams
-FileName=kingcom
+echo "Quel client souhaite tu déployer ?"
+read  client
+#FileName=kingcom
 Service='ninjarmm'
 
 if ps ax | grep -v grep | grep $Service > /dev/null
@@ -13,6 +15,6 @@ else
 	mkdir /tmp/$AppName
 	cd /tmp/$AppName
 	curl -o $FileNamemainoffice-4.5.6153-installer.pkg #https://data.easy-info.com/index.php/s/sRZKmUEYvHS0U5W/download
-	sudo installer -pkg $FileNamemainoffice-4.5.6153-installer.pkg -target /
+	sudo installer -pkg $clientoffice-4.5.6153-installer.pkg -target /
 	rm -rf /tmp/$AppName
 fi
