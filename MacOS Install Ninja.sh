a589f430134ba148d2ac05b18a81ec6415e0c5be
Dijkstra92 <35580539+Dijkstra92@users.noreply.github.com>
2020-03-05 12:11:38 +0100

    Add files via upload

---
+++ MacOS Install Ninja.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Check if Teams service is running else it downloads and installs the agent
+
+AppName=Teams
+FileName=kingcom
+Service='ninjarmm'
+
+if ps ax | grep -v grep | grep $Service > /dev/null
+then
+	echo "$Service en cours de fonctionnement, Fermeture"
+else
+	mkdir /tmp/$AppName
+	cd /tmp/$AppName
+	curl -o $FileNamemainoffice-4.5.6153-installer.pkg https://data.easy-info.com/index.php/s/sRZKmUEYvHS0U5W/download
+	sudo installer -pkg $FileNamemainoffice-4.5.6153-installer.pkg -target /
+	rm -rf /tmp/$AppName
+fi
