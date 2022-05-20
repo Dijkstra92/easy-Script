552343bfc5b5bc573b822c177bca4944170c5744
Dijkstra92 <35580539+Dijkstra92@users.noreply.github.com>
2022-05-20 15:05:21 +0200

    Update sophos Deploiement.sh

--- sophos Deploiement.sh
+++ sophos Deploiement.sh
@@ -4,7 +4,7 @@
 cd /tmp/
 printf "entrez le code de Download:\n"
 read code
-curl -k -o https://data.easy-info.com/index.php/s/$code/download
+curl -o https://data.easy-info.com/index.php/s/$code/download
 unzip SophosInstall.zip
 chmod +x /tmp/Sophos\ Installer.app/Contents/Ressources/SophosBootstrap-Options.plist 
 	 /tmp/Sophos\ Installer.app/Contents/MacOS/Sophos\ Installer --install
